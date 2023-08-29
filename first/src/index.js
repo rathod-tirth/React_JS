00cc09602bb8cb8aa7a1bb0ed4ffb3e88ce19832
rathod-tirth <tirthrathod6@gmail.com>
2023-08-29 18:41:04 +0530

    React

--- first/src/index.js
+++ first/src/index.js
@@ -1,4 +1,4 @@
-// imports
+// refer readme file for more info
 
 // importing react and reactDOM files is neccecary for react
 import React from "react";
@@ -7,34 +7,63 @@ import ReactDOM from "react-dom/client";
 // importing CSS file requires to write the extensions and it imports the whole file
 import "./index.css";
 
+const books = [
+  {
+    title: "Iron Flame",
+    author: "Rebecca Yarros",
+    img: "./images/book_1.jpg",
+  },
+  {
+    title: "Fourth Wing",
+    author: "Rebecca Yarros",
+    img: "https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg",
+  },
+  {
+    title: "Atomic Habits",
+    author: "James Clear",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg",
+  },
+  {
+    title: "The Four Agreements",
+    author: "Don Miguel Ruiz",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81hHy5XrdKL._AC_UL600_SR600,400_.jpg",
+  },
+  {
+    title: "A Little Life",
+    author: "Hanya Yanagihara",
+    img: "https://images-na.ssl-images-amazon.com/images/I/91fRT+cJNzL._AC_UL600_SR600,400_.jpg",
+  },
+  {
+    title: "It Ends with Us",
+    author: "Colleen Hoover",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg",
+  },
+];
+
 // creating an function component
 function BookList() {
   return (
     // className is used instead of class
-    <section className="booklist">
-      {/* multiple component can be render, but must have one parent component */}
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
+    <section className="booklist"></section>
   );
 }
 
-const author = "Rebecca Yarros";
-
 // creating a arrow function component
-const Book = () => {
-  const title = "Iron Flame";
+const Book = (props) => {
+  // props is an object passed in an component, which is useful to make our code dynamic
+
+  // destructuring the props object to optimize our code
+  const { title, author, img } = props;
+  console.log(props);
   return (
     <article className="book">
-      <img src="./images/book_1.jpg" alt="Iron Flame" />
+      <img src={img} alt={title} />
       {/* {} in JSX means going back to JS Land 
           value inside must be an expression (return value), can't be a statement */}
       <h2>{title}</h2>
-      <h4>{author.toUpperCase()}</h4>
+      <h4>{author}</h4>
+      {/* {children} */}
+      {/* children props can only be accessed by writing the childern as the object parameter */}
     </article>
   );
 };
