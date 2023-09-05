9ae326a94a96ccb73498130ae06deddb544c2fdb
rathod-tirth <tirthrathod6@gmail.com>
2023-09-05 17:44:12 +0530

    React

--- first/src/Data.js
+++ first/src/Data.js
@@ -1,38 +1,45 @@
+import book1 from "./Images/book_1.jpg";
+import book2 from "./Images/book_2.jpg";
+import book3 from "./Images/book_3.jpg";
+import book4 from "./Images/book_4.jpg";
+import book5 from "./Images/book_5.jpg";
+import book6 from "./Images/book_6.jpg";
+
 export const books = [
     {
         id: 1,
         title: "Iron Flame",
         author: "Rebecca Yarros",
-        img: "./images/book_1.jpg",
+        img: book1,
     },
     {
         id: 2,
         title: "Fourth Wing",
         author: "Rebecca Yarros",
-        img: "https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg",
+        img: book2,
     },
     {
         id: 3,
         title: "Atomic Habits",
         author: "James Clear",
-        img: "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg",
+        img: book3,
     },
     {
         id: 4,
         title: "The Four Agreements",
         author: "Don Miguel Ruiz",
-        img: "https://images-na.ssl-images-amazon.com/images/I/81hHy5XrdKL._AC_UL600_SR600,400_.jpg",
+        img: book4,
     },
     {
         id: 5,
         title: "A Little Life",
         author: "Hanya Yanagihara",
-        img: "https://images-na.ssl-images-amazon.com/images/I/91fRT+cJNzL._AC_UL600_SR600,400_.jpg",
+        img: book5,
     },
     {
         id: 6,
         title: "It Ends with Us",
         author: "Colleen Hoover",
-        img: "https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg",
+        img: book6,
     },
 ];
