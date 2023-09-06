683e0e06ffd8c43476222c93eba776c9c3f87f18
rathod-tirth <tirthrathod6@gmail.com>
2023-09-06 22:40:14 +0530

    Adv-react

--- adv-react/src/tutorial/01-useState/starter/03-Array.jsx
+++ adv-react/src/tutorial/01-useState/starter/03-Array.jsx
@@ -4,11 +4,6 @@ import { data } from "../../../data";
 const Array = () => {
   const [people, setPeople] = useState(data);
 
-  const keep = (id) => {
-    let newList = people.filter((ppl) => ppl.id === id);
-    setPeople(newList);
-  };
-
   const remove = (id) => {
     let newList = people.filter((ppl) => ppl.id !== id);
     setPeople(newList);
@@ -22,7 +17,6 @@ const Array = () => {
           return (
             <div key={id}>
               <h4>{name}</h4>
-              <button className="btn" onClick={() => { keep(id) }} style={{ margin: "0 1rem" }}>Keep</button>
               <button className="btn" onClick={() => { remove(id) }}>Remove</button>
             </div>
           );
