1e55a39fe006521a8455147083e96732e575e11b
rathod-tirth <tirthrathod6@gmail.com>
2023-09-07 17:06:54 +0530

    Adv-react

--- adv-react/src/App.jsx
+++ adv-react/src/App.jsx
@@ -1,5 +1,8 @@
-// import Starter from "./tutorial/01-useState/starter/01-Error"
-import Starter from "./tutorial/01-useState/starter/05-Gotcha"
+// import Starter from "./tutorial/01-useState/01-Error"
+// import Starter from "./tutorial/01-useState/02-Basics"
+// import Starter from "./tutorial/01-useState/03-Array"
+// import Starter from "./tutorial/01-useState/04-Object"
+import Starter from "./tutorial/01-useState/05-Gotcha"
 
 function App() {
 
