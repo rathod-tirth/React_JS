cf2502b0309dff148b8a34443231dd0d301622e7
rathod-tirth <tirthrathod6@gmail.com>
2023-09-11 18:25:34 +0530

    Adv-react

--- adv-react/src/App.jsx
+++ adv-react/src/App.jsx
@@ -1,9 +1,11 @@
+// useState
 // import Starter from "./tutorial/01-useState/01-Error"
 // import Starter from "./tutorial/01-useState/02-Basics"
 // import Starter from "./tutorial/01-useState/03-Array"
 // import Starter from "./tutorial/01-useState/04-Object"
 // import Starter from "./tutorial/01-useState/05-Gotcha"
 
+// useEffect
 // import Starter from "./tutorial/02-useEffect/starter/01-Example"
 // import Starter from "./tutorial/02-useEffect/starter/02-Basics"
 // import Starter from "./tutorial/02-useEffect/starter/03-Multiple"
