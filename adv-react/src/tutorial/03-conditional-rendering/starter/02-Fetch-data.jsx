ca6bd6a08a158377000798c690fb15a102608985
rathod-tirth <tirthrathod6@gmail.com>
2023-09-18 18:06:53 +0530

    update code

--- adv-react/src/tutorial/03-conditional-rendering/starter/02-Fetch-data.jsx
+++ adv-react/src/tutorial/03-conditional-rendering/starter/02-Fetch-data.jsx
@@ -4,26 +4,24 @@ const url = 'https://api.github.com/users/QuincyLarson';
 // multiple returns
 const FetchData = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false)
+  const [isError, setIsError] = useState(false);
   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const api = async () => {
-      try {
-        const res = await fetch(url);
-        if (!res.ok) {
-          throw new Error(`There was an error: ${res.status}`);
-        }
-        const data = await res.json();
-        setUser(data);
-      } catch (err) {
-        setIsError(true);
-        console.log(err);
+  const api = async () => {
+    try {
+      const res = await fetch(url);
+      if (!res.ok) {
+        throw new Error(`There was an error: ${res.status}`);
       }
-      setIsLoading(false);
-    };
-    api();
-  }, []);
+      const data = await res.json();
+      setUser(data);
+    } catch (err) {
+      setIsError(true);
+      console.log(err);
+    }
+    setIsLoading(false);
+  };
+  useEffect(() => { api() }, []);
 
   if (isLoading) {
     return <h3>Loading...</h3>
@@ -31,14 +29,28 @@ const FetchData = () => {
   if (isError) {
     return <h3>Error Fetching the data</h3>
   }
+  const { avatar_url, name, company, bio } = user;
   return (
     <>
-      <img style={{ width: "150px", borderRadius: "25px" }} src={user.avatar_url} alt={user.name} />
-      <h3>{user.name}</h3>
-      <h5>{user.company}</h5>
-      <p>{user.bio}</p>
+      <img style={{ width: "150px", borderRadius: "25px" }} src={avatar_url} alt={name} />
+      <h3>{name}</h3>
+      <h5>{company}</h5>
+      <p>{bio}</p>
     </>
   );
 
 };
 export default FetchData;
+
+/**
+ * there are multiple ways of writing the code above
+ * ex. 1. remove isLoading and use (!user) instead
+ *     2. destructure above by giving "{}" to user, etc.
+ * 
+ * three states:
+ *  1. Loading
+ *  2. Error
+ *  3. Success
+ * 
+ * 
+ */
