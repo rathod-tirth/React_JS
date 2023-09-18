034236f5af14d0a60616f666d7210e12f177de6d
rathod-tirth <tirthrathod6@gmail.com>
2023-09-18 19:10:38 +0530

    update code

--- adv-react/src/tutorial/03-conditional-rendering/starter/03-Short-circuit.jsx
+++ adv-react/src/tutorial/03-conditional-rendering/starter/03-Short-circuit.jsx
@@ -1,6 +1,26 @@
 import { useState } from 'react';
 
-const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+const ShortCircuit = () => {
+  const [text, setText] = useState("");
+  const [name, setName] = useState("james");
+
+  setTimeout(() => {
+    setText(`Greetings`);
+    setName(`susan`);
+  }, 3000)
+
+  return (
+    <>
+      <h4>{text || `Hello There`}</h4>
+      <h4>{text && `Hello There`}</h4>
+      <h4>{name || `Hello There`}</h4>
+      <h4>{name && `Hello There`}</h4>
+    </>
+  );
 };
-export default ShortCircuitOverview;
+export default ShortCircuit;
+
+/**
+ * if condition doesn't work inside jsx
+ * so we use short circuit evalution
+ */
