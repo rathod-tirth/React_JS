825b47b521a7ab7b4da15edaa1004b2a5d329849
rathod-tirth <tirthrathod6@gmail.com>
2023-10-22 15:55:37 +0530

    add remove btn

--- projects/tours/src/Tour.jsx
+++ projects/tours/src/Tour.jsx
@@ -1,14 +1,20 @@
-function Tour({ tour }) {
-   const { image, info, name, price } = tour;
+import { useState } from "react"
+
+function Tour({ id, image, info, name, price, removeTour }) {
+   const [isMore, setIsMore] = useState(false);
    return (
-      <>
-         <div>
-            <img src={image} alt="img" />
-            <h2>{name}</h2>
-            <h3>${price}</h3>
-            <p>{info}</p>
+      <article className="single-tour">
+         <img src={image} alt={name} className="img" />
+         <span className="tour-price">₹{price}</span>
+         <div className="tour-info">
+            <h5>{name}</h5>
+            <p>
+               {isMore ? info : `${info.slice(0, 200)}...`}
+               <button className="info-btn" onClick={() => { setIsMore(!isMore) }}>{isMore ? `Show Less` : `Read More`}</button>
+            </p>
+            <button className="delete-btn btn-block btn" onClick={() => { removeTour(id) }}>Remove</button>
          </div>
-      </>
+      </article>
    )
 }
 export default Tour
