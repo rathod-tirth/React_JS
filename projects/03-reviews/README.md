5d6e6e3b024c168cdcb1f8ef2afddbdfd7e07c3e
rathod-tirth <tirthrathod6@gmail.com>
2023-11-09 18:49:56 +0530

    project compelete

--- projects/03-reviews/README.md
+++ projects/03-reviews/README.md
@@ -56,7 +56,7 @@ The modulus operator in JavaScript is represented by the percent sign (%). It re
 Overall, the flow of the application should look something like this:
 
 - Import the reviews data into your project as an array of objects.
-- Set up the reviews data as a state variable using the useState hook.
+- Set up the Index as a state variable using the useState hook.
 - Render the first person's review in the list using their image, name, job, and text properties.
 - Set up buttons to display the next and previous reviews in the list. Keep track of the current index in the reviews array and update it when the user clicks the next or previous button.
 - Set up a button to display a random person's review. This button should select a random index in the reviews array and use it to display the corresponding person's review.
--- projects/03-reviews/src/App.jsx
+++ projects/03-reviews/src/App.jsx
@@ -1,39 +1,50 @@
 import { useState } from "react";
 import reviews from "./data";
-import { FaBeer } from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 function App() {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = reviews[index];
 
   const Next = () => {
-    if (index === reviews.length) {
+    if (index === reviews.length - 1) {
       setIndex(0);
     } else {
       setIndex(index + 1);
     }
   };
 
-  // const Prev = (num) => {
-  //   if (num === 0) {
-  //     setIndex(reviews.length);
-  //   } else {
-  //     setIndex(index - 1);
-  //   }
-  // };
+  const Prev = () => {
+    if (index === 0) {
+      setIndex(reviews.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
+
+  const random = () => {
+    setIndex(Math.floor(Math.random() * reviews.length));
+  };
 
   return (
-    <>
-      <h2>Reviews Starter</h2>
-      <div>
-        <h3>{name}</h3>
-        <h4>{job}</h4>
-        <img src={image} alt={name} />
-        <p>{text}</p>
-      </div>
-      {/* <button onClick={() => Prev()}>Prev</button> */}
-      <button onClick={() => Next()}>Next</button>
-    </>
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img className="person-img" src={image} alt={name} />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h3 className="author">{name}</h3>
+        <h4 className="job">{job}</h4>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={() => Prev()}><FaChevronLeft /></button>
+          <button className="next-btn" onClick={() => Next()}><FaChevronRight /></button>
+        </div>
+        <button className="btn btn-hipster" onClick={() => random()}>Random</button>
+      </article>
+    </main>
   );
 }
 
