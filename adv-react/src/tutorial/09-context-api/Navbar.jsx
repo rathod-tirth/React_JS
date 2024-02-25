7168cdd66912b534a77b426351df8e863d6e0d8f
rathod-tirth <tirthrathod6@gmail.com>
2024-02-25 22:25:14 +0530

    add comment

--- adv-react/src/tutorial/09-context-api/Navbar.jsx
+++ adv-react/src/tutorial/09-context-api/Navbar.jsx
@@ -1,8 +1,11 @@
 import { createContext, useContext, useState } from "react"
 import Navlink from "./Navlink"
 
+// creating a new context
 export const NavContext = createContext();
 
+// to use the context we use useContext
+// here to reduce the code we create a function which calls the useContext
 export const useNavContext = () => useContext(NavContext);
 
 function Navbar() {
@@ -13,6 +16,7 @@ function Navbar() {
    }
 
    return (
+      // giving range and value for out context
       <NavContext.Provider value={{ user, logOut }}>
          <nav className="navbar">
             <h5>Navbar</h5>
@@ -22,3 +26,18 @@ function Navbar() {
    )
 }
 export default Navbar
+
+/**
+ * The createContext declares our context, later we provide it with a range
+ * and value to work with in the return section.
+ * 
+ * The context is created and defined in the parent class and every single child 
+ * components can access the values.
+ * 
+ * This replaces prop drilling and makes passing values to child components easier.
+ * 
+ * To use the context we use useContext which fetches the values provided in the context.
+ * 
+ * We can also just declare our own function which instead calls the useContext and fetches the data
+ * and then call that function to reduce the code, like done in above program. 
+ */
