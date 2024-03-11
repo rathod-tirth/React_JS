2f9f6e93afb6063de8971a66ad412a863149c88e
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-11 20:05:22 +0530

    update usereducer in new file with organized code

--- adv-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ adv-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,26 +1,10 @@
 import { useReducer } from 'react';
 import { data } from '../../../data';
-
-const CLEAR_LIST = 'CLEAR_LIST';
-const RESET_LIST = 'RESET_LIST';
-const REMOVE_ITEM = 'REMOVE_ITEM';
+import { CLEAR_LIST, REMOVE_ITEM, RESET_LIST } from "./action"
+import reducer from './reducer';
 
 const defaultState = {
   people: data,
-}
-
-const reducer = (state, action) => {
-  if (action.type === CLEAR_LIST) {
-    return { ...state, people: [] }
-  }
-  if (action.type === RESET_LIST) {
-    return { ...state, people: data }
-  }
-  if (action.type === REMOVE_ITEM) {
-    let newPeople = state.people.filter(person => person.id !== action.payload.id)
-    return { ...state, people: newPeople }
-  }
-  throw new Error(`No matching "${action.type}" - action.type`);
 }
 
 const ReducerBasics = () => {
