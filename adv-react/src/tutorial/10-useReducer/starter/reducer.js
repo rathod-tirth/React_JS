b95e6a03ae0bdc883342c93de719e36b4e51bb14
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-11 20:04:25 +0530

    add reducer and action in separate file for more organized code

---
+++ adv-react/src/tutorial/10-useReducer/starter/reducer.js
@@ -0,0 +1,18 @@
+import { CLEAR_LIST, REMOVE_ITEM, RESET_LIST } from "./action"
+import { data } from "../../../data"
+
+const reducer = (state, action) => {
+   if (action.type === CLEAR_LIST) {
+      return { ...state, people: [] }
+   }
+   if (action.type === RESET_LIST) {
+      return { ...state, people: data }
+   }
+   if (action.type === REMOVE_ITEM) {
+      let newPeople = state.people.filter(person => person.id !== action.payload.id)
+      return { ...state, people: newPeople }
+   }
+   throw new Error(`No matching "${action.type}" - action.type`);
+}
+
+export default reducer
