3f6f1b059e72ca7bcecbc2eaf6f38606c7344df9
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-11 18:55:23 +0530

    add useReducer

--- adv-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ adv-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import { useReducer } from 'react';
 import { data } from '../../../data';
 
+const CLEAR_LIST = 'CLEAR_LIST';
+const RESET_LIST = 'RESET_LIST';
+const REMOVE_ITEM = 'REMOVE_ITEM';
+
+const defaultState = {
+  people: data,
+}
+
+const reducer = (state, action) => {
+  if (action.type === CLEAR_LIST) {
+    return { ...state, people: [] }
+  }
+  if (action.type === RESET_LIST) {
+    return { ...state, people: data }
+  }
+  if (action.type === REMOVE_ITEM) {
+    let newPeople = state.people.filter(person => person.id !== action.payload.id)
+    return { ...state, people: newPeople }
+  }
+  throw new Error(`No matching "${action.type}" - action.type`);
+}
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    dispatch({ type: REMOVE_ITEM, payload: { id } })
   };
+
+  const clearList = () => {
+    dispatch({ type: CLEAR_LIST })
+  }
+
+  const resetList = () => {
+    dispatch({ type: RESET_LIST })
+  }
 
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className='item'>
@@ -20,12 +50,12 @@ const ReducerBasics = () => {
           </div>
         );
       })}
-      {people.length ? (
-        <button className='btn' style={{ marginTop: '2rem' }} onClick={() => setPeople([])} >
+      {state.people.length ? (
+        <button className='btn' style={{ marginTop: '2rem' }} onClick={clearList} >
           Clear
         </button>
       ) : (
-        <button className='btn' style={{ marginTop: '2rem' }} onClick={() => setPeople(data)} >
+        <button className='btn' style={{ marginTop: '2rem' }} onClick={resetList} >
           Reset
         </button>
       )}
@@ -35,3 +65,28 @@ const ReducerBasics = () => {
 };
 
 export default ReducerBasics;
+
+
+/**
+ * useReducer is used for state management, it is an hook provided by react itself.
+
+ * useReducer takes two arguments. 1. reducer function, 2. defaultState
+ * defaultstate is where all the state values are stored
+ * the reducer function is used to manipulate state values
+
+ * useReducer returns two values. 1. state 2. dispatch
+ * through state you can access all the state values
+ * through dispatch you can invoke a action from the reducer function
+ * 
+ * the reducer function passed in useReducer takes has two parameter
+ * 1. current state value, 2. action
+ * 
+ * we use dispatch function to pass the action type
+ * in reducer function we check for the correct action type and
+ * on based on the action type the we will update the state
+ * 
+ * whatever the reducer function will return will be the new state value
+ * now due to that we can set up a default return statement which return the current state
+ * or we can throw an error for better bug solving 
+ * if there is action which is not handled in the reducer function
+ */
