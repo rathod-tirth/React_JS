1fe7b46a84306717691724d14ae9688713c4c9a2
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-21 18:56:22 +0530

    add error 404 page not found handling

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
+import Error from './pages/Error'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div><Home /></div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   )
--- react-router/src/pages/Error.jsx
+++ react-router/src/pages/Error.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
     <section className='section'>
-      <h2>Error</h2>
+      <h2>Page not found</h2>
+      <h4>Error 404</h4>
+      <Link to="/" className="btn" >Home</Link>
     </section>
   );
 };
