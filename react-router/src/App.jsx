5d48fef6f4a9ad492b011d5c56ff5bcee4542796
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-21 12:28:21 +0530

    update js to jsx

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Hello</div>} />
+        <Route path="/" element={<div><Home /></div>} />
       </Routes>
     </BrowserRouter>
   )
