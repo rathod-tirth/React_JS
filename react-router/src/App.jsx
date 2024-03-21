4b663fc3b9ebc4dea0e6323476c014195f2a66ca
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-21 12:23:25 +0530

    add react-router

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,9 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 function App() {
 
   return (
-    <>
-      Hello
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Hello</div>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
