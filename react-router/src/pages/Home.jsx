e12706e9d6e43d19009e79ae6b6ff62c98cc0c73
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-22 19:03:55 +0530

    add sharedlayout with outlet and add index page for default route

--- react-router/src/pages/Home.jsx
+++ react-router/src/pages/Home.jsx
@@ -1,11 +1,8 @@
-import { Link } from "react-router-dom";
-
-const Home = () => {
-  return (
-    <section className='section'>
-      <h2>Home Page</h2>
-      <Link to="about" className="btn" >About</Link>
-    </section>
-  );
-};
-export default Home;
+function Home() {
+   return (
+      <section className="section">
+         <h2>Home</h2>
+      </section>
+   )
+}
+export default Home
---
+++ react-router/src/pages/SharedLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <section className='section'>
+        <Outlet />
+      </section>
+    </>
+  );
+};
+export default Home;
