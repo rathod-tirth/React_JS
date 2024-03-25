08d94a122a80574c86067af57733a960a452c93f
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-25 16:56:28 +0530

    add dashboard page

--- react-router/src/pages/Dashboard.jsx
+++ react-router/src/pages/Dashboard.jsx
@@ -1,7 +1,8 @@
-const Dashboard = () => {
+const Dashboard = ({ user }) => {
   return (
     <section className='section'>
       <h4>Dashboard</h4>
+      <h2>Hello, {user?.name || "There"}</h2>
     </section>
   );
 };
