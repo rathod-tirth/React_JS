1d2adf6ef70d2d7247b0f33a215989b9b3ed3a92
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-25 16:26:04 +0530

    add single product page and add all the product in the product page

--- react-router/src/pages/Products.jsx
+++ react-router/src/pages/Products.jsx
@@ -1,8 +1,21 @@
+import { Link } from 'react-router-dom';
+import products from '../data'
+
 const Products = () => {
   return (
     <>
       <section className='section'>
         <h2>products</h2>
+        <div className="products">
+          {products.map((item) => {
+            return (
+              <article key={item.id}>
+                <h5>{item.name}</h5>
+                <Link to={`/products/${item.id}`} >More info</Link>
+              </article>
+            )
+          })}
+        </div>
       </section>
     </>
   );
--- react-router/src/pages/SingleProduct.jsx
+++ react-router/src/pages/SingleProduct.jsx
@@ -1,10 +1,14 @@
 import { Link, useParams } from "react-router-dom";
+import products from "../data";
 
 const SingleProduct = () => {
   const { proId } = useParams()
+  const newData = products.find((product) => product.id === proId)
+
   return (
     <section className='section product'>
-      <h2>{proId}</h2>
+      <img src={newData.image} alt={newData.name} />
+      <h2>{newData.name}</h2>
       <Link to="/products" className="btn" >Back</Link>
     </section>
   );
