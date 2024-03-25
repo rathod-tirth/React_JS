fb6b7f6bd5103116ab7b5e9b0863b2ebeed79af7
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-25 16:26:21 +0530

    update image path

--- react-router/src/data.js
+++ react-router/src/data.js
@@ -3,34 +3,34 @@ const products = [
     id: 'recZkNf2kwmdBcqd0',
     name: 'accent chair',
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681749482/react-comfy-store-products/iuYyO9RP_o_upinxq.jpg',
+      'https://m.media-amazon.com/images/I/71q32o2in-L.jpg',
   },
   {
     id: 'recEHmzvupvT8ZONH',
     name: 'albany sectional',
 
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750782/react-comfy-store-products/product-2_lusrzx.jpg',
+      'https://cdn11.bigcommerce.com/s-mhjjd3e/images/stencil/1920x1280/products/2469/10246/albany-938-tweed-silver-sectional__75327.1702062851.jpg?c=2',
   },
   {
     id: 'rec5NBwZ5zCD9nfF0',
     name: 'albany table',
 
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750874/react-comfy-store-products/product-3_znpiqa.jpg',
+      'https://www.earthgarden.in/cdn/shop/products/AlbanyExtensionTable5_570x.jpg?v=1665240603',
   },
   {
     id: 'recd1jIVIEChmiwhe',
     name: 'armchair',
 
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750929/react-comfy-store-products/product-4_ebl6q1.jpg',
+      'https://www.gulmoharlane.com/uploads/gulmoharlane/products/dalhousie-tufted-leather-armchair---1-337888_l.jpg?v=366',
   },
   {
     id: 'recoM2MyHJGHLVi5l',
     name: 'bar stool',
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681751026/react-comfy-store-products/product-5_n184nu.jpg',
+      'https://i.ebayimg.com/images/g/gLoAAOSwOJVjLABW/s-l1600.jpg',
   },
 ];
 
