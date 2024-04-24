38fd02841eea36a91484dd9d24d6b0e59a168c07
Tirth <tirth.rathod@naapbooks.com>
2024-04-24 11:16:08 +0530

    imporve performance by pushing state down the tree

--- adv-react/src/App.jsx
+++ adv-react/src/App.jsx
@@ -44,7 +44,8 @@
 // import Starter from "./tutorial/10-useReducer/01-useReducer"
 
 // performance
-import Starter from './tutorial/11-performance/starter/01-lower-state/index'
+// import Starter from './tutorial/11-performance/starter/01-lower-state/index'
+import Starter from './tutorial/11-performance/starter/02-lower-state-challenge/index'
 
 function App() {
 
