3ec3b5819138a0356e4f064442ed732b8e2c2c59
Tirth <tirth.rathod@naapbooks.com>
2024-04-25 18:49:27 +0530

    add template with store value

--- redux-toolkit/vite-starter/src/App.jsx
+++ redux-toolkit/vite-starter/src/App.jsx
@@ -1,4 +1,12 @@
+import CartContainer from "./Components/CartContainer";
+import Navbar from "./Components/Navbar";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
 }
 export default App;
