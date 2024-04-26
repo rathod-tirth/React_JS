c5edbcee9072ece4a99fa71026b230c62cf8b3d2
Tirth <tirth.rathod@naapbooks.com>
2024-04-26 18:50:40 +0530

    add reducer in the slice

--- redux-toolkit/vite-starter/src/Features/Cart/cartSlice.js
+++ redux-toolkit/vite-starter/src/Features/Cart/cartSlice.js
@@ -11,9 +11,35 @@ const initialState = {
 const cartSlice = createSlice({
    name: 'cart',
    initialState,
+   reducers: {
+      clearCart: (state) => {
+         state.cartItems = []
+      },
+      removeCart: (state, { payload }) => {
+         state.cartItems = state.cartItems.filter((item) => item.id !== payload)
+      },
+      increase: (state, { payload }) => {
+         const cartItems = state.cartItems.find((item) => item.id === payload)
+         cartItems.amount += 1
+      },
+      decrease: (state, { payload }) => {
+         const cartItems = state.cartItems.find((item) => item.id === payload)
+         if (cartItems.amount > 0) {
+            cartItems.amount -= 1
+         }
+      },
+      total: (state) => {
+         // const cartItems = state.cartItems.find((item) => item.id === payload)
+         // let total = cartItems.amount * cartItems.price
+         // state.total += total
+         let total = 0
+         // state.cartItems.
+      }
+   }
 })
 
 // console.log(cartSlice);
 // console.log(cartSlice.getInitialState().isLoading);
 
+export const { clearCart, removeCart, increase, decrease } = cartSlice.actions
 export default cartSlice.reducer
