63efd1fb6ca6623cb9be49c8fa84637b0885cdc1
Tirth <tirth.rathod@naapbooks.com>
2024-04-26 18:51:24 +0530

    add dispatch for clearCart

--- redux-toolkit/vite-starter/src/Components/CartContainer.jsx
+++ redux-toolkit/vite-starter/src/Components/CartContainer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from '../Features/Cart/cartSlice';
 
 const CartContainer = () => {
    const { cartItems, amount, total } = useSelector((store) => store.cart)
+   const dispatch = useDispatch()
 
    if (amount < 1) {
       return (
@@ -36,7 +38,7 @@ const CartContainer = () => {
                   total <span>${total}</span>
                </h4>
             </div>
-            <button className='btn clear-btn'>clear cart</button>
+            <button className='btn clear-btn' onClick={() => dispatch(clearCart())} >clear cart</button>
          </footer>
       </section>
    );
