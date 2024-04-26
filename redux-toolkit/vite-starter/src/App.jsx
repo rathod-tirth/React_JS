c1494bf29029ac8bb155fd45f43dcfd338226f5f
Tirth <tirth.rathod@naapbooks.com>
2024-04-26 19:16:10 +0530

    dispatch calculateTotal reducer in useEffect

--- redux-toolkit/vite-starter/src/App.jsx
+++ redux-toolkit/vite-starter/src/App.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import CartContainer from "./Components/CartContainer";
 import Navbar from "./Components/Navbar";
+import { calculateTotal } from "./Features/Cart/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(calculateTotal())
+  }, [cartItems])
   return (
     <main>
       <Navbar />
