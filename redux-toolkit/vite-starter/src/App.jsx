2a0ac9cc0091fc0964f481fb7c72e4315a0cdcec
Tirth <tirth.rathod@naapbooks.com>
2024-04-30 18:45:35 +0530

    add async data loading in useffect

--- redux-toolkit/vite-starter/src/App.jsx
+++ redux-toolkit/vite-starter/src/App.jsx
@@ -1,21 +1,34 @@
 import { useEffect } from "react";
 import CartContainer from "./Components/CartContainer";
 import Navbar from "./Components/Navbar";
-import { calculateTotal } from "./Features/Cart/cartSlice";
+import { calculateTotal, getCartItems } from "./Features/Cart/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
 import Modal from "./Components/Modal";
 
 function App() {
-  const { cartItems } = useSelector((store) => store.cart)
+  const { cartItems, isLoading } = useSelector((store) => store.cart)
+  const { isOpen } = useSelector((store) => store.modal)
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getCartItems())
+  }, [])
 
   useEffect(() => {
     dispatch(calculateTotal())
   }, [cartItems])
 
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
   return (
     <main>
-      <Modal />
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
