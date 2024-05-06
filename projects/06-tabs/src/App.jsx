00f3387716df1ba098647b2b435ceadc0b4134a0
Tirth <tirth.rathod@naapbooks.com>
2024-05-06 15:03:53 +0530

    add isloading fetch the job data and import components

--- projects/06-tabs/src/App.jsx
+++ projects/06-tabs/src/App.jsx
@@ -1,6 +1,40 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import JobInfo from "./JobInfo";
+import BtnContainer from "./BtnContainer";
+
 const url = 'https://www.course-api.com/react-tabs-project';
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  const [data, setData] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [currentItem, setCurrentItem] = useState(0)
+
+  const handleFetch = async () => {
+    try {
+      const resp = await fetch(url)
+      const newData = await resp.json()
+      setData(newData)
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => { handleFetch() }, [])
+
+  if (isLoading) {
+    return (
+      <section className='jobs-center'>
+        <div className='loading'></div>
+      </section>
+    )
+  }
+  return (
+    <section className="jobs-center">
+      <BtnContainer data={data} setCurrentItem={setCurrentItem} currentItem={currentItem} />
+      <JobInfo data={data} currentItem={currentItem} />
+    </section>
+  )
 };
 export default App;
