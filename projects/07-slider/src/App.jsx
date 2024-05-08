c2bb218dcba73f1f21de9bd5bd47b6bd8f569b9d
Tirth <tirth.rathod@naapbooks.com>
2024-05-08 11:29:01 +0530

    add carousel logic

--- projects/07-slider/src/App.jsx
+++ projects/07-slider/src/App.jsx
@@ -1,7 +1,10 @@
+import Carousel from "./Carousel";
+
 const App = () => {
   return (
     <main>
-      <h2>Slider Starter</h2>
+      <h2 className="title">Slider Starter</h2>
+      <Carousel />
     </main>
   );
 };
--- projects/07-slider/src/Carousel.jsx
+++ projects/07-slider/src/Carousel.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { list, shortList } from './data'
-import { useState } from 'react'
+import { FaQuoteRight } from 'react-icons/fa';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 const Carousel = () => {
-   const [data, setData] = useState(shortList)
+   const [data, setData] = useState(list)
+   const [currentPerson, setCurrentPerson] = useState(0)
+
+   const handlePrev = () => {
+      if (currentPerson > 0) {
+         setCurrentPerson(currentPerson - 1)
+      } else {
+         setCurrentPerson(data.length - 1)
+      }
+   }
+
+   const handleNext = () => {
+      if (currentPerson < data.length - 1) {
+         setCurrentPerson(currentPerson + 1)
+      } else {
+         setCurrentPerson(0)
+      }
+   }
+
+   useEffect(() => {
+      let sliderInterval = setInterval(() => { handleNext() }, 5000)
+      return () => { clearInterval(sliderInterval) }
+   }, [currentPerson])
 
    return (
-      <div>Carousel</div>
+      <div>
+         <div className='slider-container' >
+            {data.map((item, index) => {
+               if (currentPerson === index) {
+                  return (
+                     <article className='slide' key={item.id} >
+                        <img src={item.image} alt={item.name} className='person-img' />
+                        <h5 className='name'>{item.name}</h5>
+                        <p className='title'>{item.title}</p>
+                        <p className='text'>{item.quote}</p>
+                        <FaQuoteRight className='icon' />
+                     </article>
+                  )
+               }
+            })}
+         </div>
+         <button className='prev' onClick={() => handlePrev()}><FiChevronLeft /></button>
+         <button className='next' onClick={() => handleNext()}><FiChevronRight /></button>
+      </div>
    )
 }
 
